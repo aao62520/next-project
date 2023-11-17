cdd3fad2014f722e89f9beca04c0ecbb26030bfd
Ahmad Odeh <aao62@njit.edu>
2023-11-17 10:51:06 -0500

    old repo

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,47 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+
+
+
+Ahmad Odeh is a UX designer with a deep-seated passion for crafting user-centric experiences that seamlessly blend functionality with aesthetics. With a keen eye for detail and an unwavering commitment to user empathy, Ahmad has successfully navigated the complexities of the UX design landscape, consistently delivering products that are both intuitive and engaging.</p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>{title}</Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
